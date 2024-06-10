2595c68178a8804a14d7c79f8bdaa70e8755d240
ST10440057 <ST10440057@vcconnect.edu.za>
2024-06-10 08:27:09 +0200

    added splash screen

--- src/main/java/com/st10440057/st10440057_imad5112_practicum/SplashScreen.kt
+++ src/main/java/com/st10440057/st10440057_imad5112_practicum/SplashScreen.kt
@@ -3,7 +3,7 @@ package com.st10440057.st10440057_imad5112_practicum
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class SplashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
