9562a08b8cf8544b005eaec76ec4cd9390dc4731
ST10440057 <ST10440057@vcconnect.edu.za>
2024-06-10 08:46:05 +0200

    added splash screen logic

--- src/main/java/com/st10440057/st10440057_imad5112_practicum/SplashScreen.kt
+++ src/main/java/com/st10440057/st10440057_imad5112_practicum/SplashScreen.kt
@@ -1,11 +1,26 @@
 package com.st10440057.st10440057_imad5112_practicum
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class SplashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
-}
+
+        val HomeButton = findViewById<Button>(R.id.btnHome)     //linking buttons from home screen
+        val ExitButton = findViewById<Button>(R.id.btnExit)
+
+        HomeButton.setOnClickListener {
+            val intent = Intent(this@SplashScreen, HomePage::class.java)
+            startActivity(intent)
+
+
+            }//end of HomeButton
+        ExitButton.setOnClickListener { finish() } //closing application
+
+
+    }//end of override
+}//end of main
